4f89cc5a4f2f9d5fad1b51e5e2e63b64a288dd25
koston022 <koston022@mail.ru>
2022-09-26 22:36:08 +0500

    added static method connect to db

--- model/db.php
+++ model/db.php
@@ -1,7 +1,12 @@
 <?php
 function dbConnect(){
-    $db = new PDO('mysql:host=localhost;dbname=mts_test_task', 'root', '');
-    $db->exec('SET NAMES UTF8');
+    static $db;
+    if($db === null) {
+        $db = new PDO('mysql:host=localhost;dbname=mts_test_task', 'root', '',
+        [PDO::ATTR_DEFAULT_FETCH_MODE=>PDO::FETCH_ASSOC]
+        );
+        $db->exec('SET NAMES UTF8');
+    }
     return $db;
 }
 
