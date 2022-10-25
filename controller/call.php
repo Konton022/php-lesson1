efd1fdf64c4b9de8230ea4091a0b6564c3c52adf
koston022 <koston022@mail.ru>
2022-10-25 17:13:51 +0500

    added edit button

--- controller/call.php
+++ controller/call.php
@@ -9,7 +9,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $del_id = $_POST['del_id'];
     $delIndex = delCall($del_id);
     
-    if ($delIndex) {
+    if ($del_id) {
         header('Location:index.php');
     }  else {
         alarm('somethink went wrong');
