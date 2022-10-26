cd0e6da1d3b824522bf707231f6850403eb8bf64
koston022 <koston022@mail.ru>
2022-10-26 14:49:49 +0500

    added some views

--- view/base/v_main.php
+++ view/base/v_main.php
@@ -8,8 +8,8 @@
     <title><?=$title?></title>
 </head>
     <body>
-    <nav class="navbar navbar-expand-lg navbar-light bg-secondary text-white">
-        <div class="container-fluid">
+    <nav class="navbar navbar-expand-lg navbar-dark bg-secondary ">
+        <div class="container-fluid ">
             <a class="navbar-brand" href="#">Calls</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
--- view/messages/v-add.php
+++ view/messages/v-add.php
@@ -1,4 +1,12 @@
 <form class="form" method="POST">
-            <input class="form-control my-3" type="text" name="phone" id="phone" />
+    <div class="row m-3">
+        <div class="col-9">
+            <input class="form-control" type="text" name="phone" id="phone" />
+        </div>
+        <div class="col-3">
+
             <button type="submit" class="btn btn-primary"> send </button>
+        </div>
+
+    </div>
 </form>
